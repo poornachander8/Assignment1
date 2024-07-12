b282ed1f982ecfeb50f64ba48be0ba3133fbd791
Poornachander9 <poornachander986@gmail.com>
2024-07-12 15:48:41 -0500

    Normalize all the line endings

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello 'Roulettech'</h1>
+      <p>This is my cooking channel</p>
+    </div>
+  );
+}
+
+export default App;
